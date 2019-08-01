77d4f845dc313517e96c8c1e85fd5802f9a9a7e9
Matt Massoodi <matt@puroxy.io>
2019-07-31 23:13:47 -0700

    minor spacing issue with FAB

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -9,7 +9,7 @@ const useStyles = makeStyles(theme => ({
   bottomRightCorner: {
 		position: 'fixed',
 		bottom: theme.spacing(2),
-    right: theme.spacing(3),
+    right: theme.spacing(2),
 	},
 }));
 
