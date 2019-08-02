7e3780f399f29cda8e4756414a00a219870cb457
Matt Massoodi <matt@puroxy.io>
2019-08-02 14:29:04 -0700

    working on dish detail page layout/design

--- src/components/search/DishDetail.js
+++ src/components/search/DishDetail.js
@@ -1,30 +1,39 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Card, CardMedia } from '@material-ui/core';
-
-import Dish from './Dish';
+import { Grid, Card, CardMedia, CardHeader, Avatar, Typography } from '@material-ui/core';
+import { teal } from '@material-ui/core/colors';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles(theme => ({
   container: {
     flex: 1,
     padding: theme.spacing(2),
+    margin: theme.spacing(2),
   },
   card: {
     flex: 1,
-    margin: theme.spacing(2),
     padding: theme.spacing(2),
   },
   media: {
     height: 0,
     paddingTop: '56.25%',
   },
+  avatar: {
+    backgroundColor: teal[500],
+  },
 }))
 
 const DishDetail = ({ match }) => {
-  const { container, card, media } = useStyles();
+  const { container, card, media, avatar } = useStyles();
   return (
     <div className={container}>
-      <Grid container>
+      <Grid container spacing={2}>
+        <Grid item xs={12}>
+          <Typography variant="h4">
+            Paella Dish
+          </Typography>
+        </Grid>
         <Grid item xs={12} sm={8}>
           <Card className={card}>
             <CardMedia
@@ -35,7 +44,13 @@ const DishDetail = ({ match }) => {
         </Grid>
         <Grid item xs={12} sm={4}>
           <Card className={card}>
-            chef deets
+            <CardHeader
+              avatar={
+                <Avatar className={avatar}>M</Avatar>
+              }
+              title="Matt Massoodi"
+              subheader={<FontAwesomeIcon icon={faStar} size="sm" />}
+            />
           </Card>
         </Grid>
         <Grid item xs={12} sm={8}>
