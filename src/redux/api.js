1b3f294c8360c0f8045712b5b884404bf3be6dce
Drew Taylor <drewtaylor888@gmail.com>
2019-08-02 12:25:51 -0700

    fixed react env variable bug

--- src/redux/api.js
+++ src/redux/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 require('dotenv').config();
 
-const baseURL = process.env.API_URL || 'http://localhost:4000';
+const baseURL = process.env.REACT_APP_API_URL || 'http://localhost:4000';
 
 export default axios.create({
     baseURL
