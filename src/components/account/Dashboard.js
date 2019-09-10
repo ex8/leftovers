52e26d6d72caaa1460d1d9f3779ab014a89e1b33
Matt M <matt@puroxy.org>
2019-09-09 21:26:51 -0700

    dishes changes

--- src/components/account/Dashboard.js
+++ src/components/account/Dashboard.js
@@ -1,90 +1,58 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Card, CardMedia, Container, Grid, Typography } from '@material-ui/core';
+import { Card, Container, Grid, Typography, Avatar } from '@material-ui/core';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUtensils, faShoppingCart, faCreditCard } from '@fortawesome/free-solid-svg-icons';
 
 const useStyles = makeStyles(theme => ({
   card: {
     flex: 1,
     padding: theme.spacing(4),
-    paddingLeft: theme.spacing(6)
   },
   container: {
     flexGrow: 1,
-    margin: theme.spacing(4)
+    margin: theme.spacing(2),
   },
   linkButton: {
     textDecoration: 'none',
+    color: 'inherit',
   },
-  media: {
-    borderRadius: '5px',
-    height: '90%',
-    flex: 1,
-    marginBottom: '5%',
-    marginTop: '5%',
-    paddingTop: '50.25%'
-  }
-}))
+}));
 
-const Dashboard = () => {
-  const { card, container, linkButton, media } = useStyles();
-  const user1 = {
-    firstName: 'drew',
-    username: 'drew',
-    address: '1079 natoma street, sf, ca 94103',
-    email: 'drew@drew.com',
-    phone: '6502696088'
-  };
-
+const Dashboard = ({ user }) => {
+  const { card, container, linkButton } = useStyles();
   return (
     <div className={container}>
       <Container>
         <Grid container spacing={3}>
           <Grid item xs={12}>
             <Typography variant="h4">
-              Hi, {user1.firstName}!
+              Hi, {user.firstName}!
             </Typography>
           </Grid>
-          <Grid item xs={12} md={2}>
-            <CardMedia
-              className={media}
-              image="https://source.unsplash.com/random"
-            />
+          <Grid item xs={12} sm={6} md={4}>
+            <Link to="/account/dishes" className={linkButton}>
+              <Card className={card}>
+                <Avatar><FontAwesomeIcon icon={faUtensils} /></Avatar>
+                <Typography variant="h5">Dishes</Typography>
+              </Card>
+            </Link>
           </Grid>
-          <Grid item xs={12} md={6}>
-            <Card className={card}>
-              <Typography paragraph>Username: {user1.username}</Typography>
-              <Typography paragraph>Address: {user1.address}</Typography>
-              <Typography paragraph>Email: {user1.email}</Typography>
-              <Typography paragraph>Phone: {user1.phone}</Typography>
-            </Card>
-          </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} sm={6} md={4}>
             <Link to="/account/orders" className={linkButton}>
               <Card className={card}>
+                <Avatar><FontAwesomeIcon icon={faShoppingCart} /></Avatar>
                 <Typography variant="h5">Orders</Typography>
               </Card>
             </Link>
           </Grid>
-          <Grid item xs={12} md={6}>
+          <Grid item xs={12} sm={6} md={4}>
             <Link to="/account" className={linkButton}>
               <Card className={card}>
+                <Avatar><FontAwesomeIcon icon={faCreditCard} /></Avatar>
                 <Typography variant="h5">Payment</Typography>
-              </Card>
-            </Link>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Link to="/account/settings" className={linkButton}>
-              <Card className={card}>
-                <Typography variant="h5">Settings</Typography>
-              </Card>
-            </Link>
-          </Grid>
-          <Grid item xs={12} md={6}>
-            <Link to="/account/dishes" className={linkButton}>
-              <Card className={card}>
-                <Typography variant="h5">Dishes</Typography>
               </Card>
             </Link>
           </Grid>
--- src/components/account/Dishes.js
+++ src/components/account/Dishes.js
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, Typography, CircularProgress, Table, Hidden, Paper, TableBody, TableHead, TableRow, TableCell, Button } from '@material-ui/core';
+import Rating from '@material-ui/lab/Rating';
+
+import api from '../../redux/api';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    flexGrow: 1,
+    padding: theme.spacing(2),
+  },
+  table: {
+    flex: 1,
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    overflowX: 'auto',
+  }
+}));
 
 const Dishes = () => {
+  const { container, table, paper } = useStyles();
+  const [dishes, setDishes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    api.get('/api/search')
+      .then(res => {
+        if (res.data.success) {
+          setDishes(res.data.dishes);
+        }
+        setLoading(false);
+      })
+      .catch(() => setDishes([]));
+  }, [])
+
   return (
-    <div>
-      account dishes
+    <div className={container}>
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h4" gutterBottom>Your Dishes</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            {loading && <CircularProgress />}
+            {!loading && (
+              <Paper className={paper}>
+                <Table className={table}>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell>Title</TableCell>
+                      <TableCell>Stock</TableCell>
+                      <Hidden xsDown><TableCell>Price</TableCell></Hidden>
+                      <Hidden smDown><TableCell>Rating</TableCell></Hidden>
+                      <TableCell>Action</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {dishes.map(dish => (
+                      <TableRow key={dish._id}>
+                        <TableCell component="th" scope="row">{dish.title}</TableCell>
+                        <TableCell>{dish.stock}</TableCell>
+                        <Hidden xsDown><TableCell>${dish.price.toFixed(2)}</TableCell></Hidden>
+                        <Hidden smDown>
+                          <TableCell><Rating size="small" value={dish.rating} readOnly /></TableCell>
+                        </Hidden>
+                        <TableCell>
+                          <Button size="small" variant="contained" color="secondary">Manage</Button>
+                        </TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </Paper>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 };
